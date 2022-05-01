9026ec51d4a77cb106057379d69f8b1d73a54e9e
jenniennie <90437083+jenniennie@users.noreply.github.com>
2022-04-30 20:37:54 -0400

    in app chat working

---
+++ ios/DerivedData2/Runner/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/flutter_chat_bubble.build/Objects-normal/arm64/flutter_chat_bubble_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/jennifernguyen/baable/ios/DerivedData2/Runner/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/flutter_chat_bubble.build/DerivedSources/flutter_chat_bubble_vers.c
