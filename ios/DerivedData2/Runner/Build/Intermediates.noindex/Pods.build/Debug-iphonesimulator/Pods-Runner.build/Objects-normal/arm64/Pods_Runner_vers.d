4db034799eadeddf8711e6152003cc380ee40e72
jenniennie <90437083+jenniennie@users.noreply.github.com>
2022-05-02 17:00:55 -0400

    pretty

--- ios/DerivedData2/Runner/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/jennifernguyen/baable/ios/DerivedData2/Runner/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
--- ios/DerivedData2/Runner/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/flutter_chat_bubble.build/Objects-normal/x86_64/flutter_chat_bubble_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/jennifernguyen/baable/ios/DerivedData2/Runner/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/flutter_chat_bubble.build/DerivedSources/flutter_chat_bubble_vers.c
