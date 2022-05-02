f31fe54abbed25027bd315f9dcae5bb59a98105d
jenniennie <90437083+jenniennie@users.noreply.github.com>
2022-05-01 22:39:03 -0400

    ugly work

--- ios/DerivedData2/Runner/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/flutter_chat_bubble.build/DerivedSources/flutter_chat_bubble_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char flutter_chat_bubbleVersionString[];
- extern const double flutter_chat_bubbleVersionNumber;
-
- const unsigned char flutter_chat_bubbleVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:flutter_chat_bubble  PROJECT:Pods-1" "\n";
- const double flutter_chat_bubbleVersionNumber __attribute__ ((used)) = (double)1.;
